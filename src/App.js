3a6b67475427b78e235bdba327383cb31e35fbc2
Aline <alinemcsantoro@gmail.com>
2020-11-24 09:58:07 -0300

    feat: refactor to accomodate onboarding pages

--- src/App.js
+++ src/App.js
@@ -5,33 +5,56 @@ import { Provider as LanguageProvider } from './context/LanguageContext';
 import GetStartedPage from './pages/GetStartedPage';
 import RecoverPasswordPage from './pages/RecoverPasswordPage';
 import DashboardPage from './pages/DashboardPage';
+import PatientOnboardingPage from './pages/PatientOnboardingPage';
+import DoctorOnboardingPage from './pages/DoctorOnboardingPage';
 import './App.css';
 
 const Routes = () => {
 	const { state: { isLoggedIn } } = useContext(AuthContext);
 
+	return <Router>{isLoggedIn ? <PrivateRoute /> : <PublicRoute />}</Router>;
+};
+
+const PublicRoute = () => {
 	return (
-		<Router>
-			{/* {isLoggedIn ? (
-				<Switch>
-					<Route path="/" exact render={() => <h1>Root</h1>}>
-						<Redirect to="/dashboard" />
-					</Route>
-					<Route path="/dashboard" exact component={DashboardPage} />
-				</Switch>
-			) : ( */}
-			<Switch>
-				<Route path="/getstarted/json" exact component={GetStartedPage} />
-				{/* //TODO: REMOVE DASHBOARD ROUTE AND UNCOMMENT ISLOGGEDIN WHEN DEPLOY */}
-				<Route path="/dashboard" exact component={DashboardPage} />
-				<Route path="/recoverpassword" exact component={RecoverPasswordPage} />
-				<Route path="/" exact render={() => <h1>Root</h1>}>
-					<Redirect to="/getstarted/json" />
-				</Route>
-				<Route render={() => <h1>ERROR NOT FOUND</h1>} />
-			</Switch>
-			{/* )} */}
-		</Router>
+		<Switch>
+			<Route path="/getstarted/json" exact component={GetStartedPage} />
+			<Route path="/dashboard" exact component={DashboardPage} />
+			<Route path="/recoverpassword" exact component={RecoverPasswordPage} />
+			<Route path="/" exact render={() => <h1>Root</h1>}>
+				<Redirect to="/getstarted/json" />
+			</Route>
+			<Route render={() => <h1>ERROR NOT FOUND</h1>} />
+		</Switch>
+	);
+};
+
+const PrivateRoute = () => {
+	const { state: { userAmIHCP } } = useContext(AuthContext);
+	return userAmIHCP ? <PrivateRouteDoctor /> : <PrivateRoutePatient />;
+};
+
+const PrivateRouteDoctor = () => {
+	return (
+		<Switch>
+			<Route path="/" exact render={() => <h1>Root</h1>}>
+				<Redirect to="/doctordashboard" />
+			</Route>
+			<Route path="/doctordashboard" exact component={DashboardPage} />
+			<Route path="/doctoronboarding" exact component={DoctorOnboardingPage} />
+		</Switch>
+	);
+};
+
+const PrivateRoutePatient = () => {
+	return (
+		<Switch>
+			<Route path="/" exact render={() => <h1>Root</h1>}>
+				<Redirect to="/patientdashboard" />
+			</Route>
+			<Route path="/patientonboarding" exact component={PatientOnboardingPage} />
+			<Route path="/patientdashboard" exact component={DashboardPage} />
+		</Switch>
 	);
 };
 
---
+++ src/pages/PatientOnboardingPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+
+const DoctorOnboardingPage = () => {
+	return (
+		<div>
+			<Navbar />
+		</div>
+	);
+};
+
+export default DoctorOnboardingPage;
