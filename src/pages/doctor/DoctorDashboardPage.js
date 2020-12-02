7d1cf206802a6921aa46fb3ad21a8c7916723fca
Aline <alinemcsantoro@gmail.com>
2020-12-02 13:34:47 -0300

    fix: reviewed code for merging

--- src/pages/doctor/DoctorDashboardPage.js
+++ src/pages/doctor/DoctorDashboardPage.js
@@ -1,18 +1,16 @@
-import React from "react";
-import AppBar from "../../components/dashboard/AppBar.js";
-import MainInfo from "../../components/dashboard/MainInfo.js";
-import TabsPage from "../../components/dashboard/Tabs.js";
+import React from 'react';
+import AppBar from '../../components/dashboard/AppBar.js';
+import MainInfo from '../../components/dashboard/MainInfo.js';
+import TabsPage from '../../components/dashboard/Tabs.js';
 
 const DoctorDashboardPage = () => {
-  return (
-    <div>
-      <AppBar>
-      </AppBar>
-      <MainInfo>
-      </MainInfo>
-      <TabsPage></TabsPage>
-    </div>
-  );
+	return (
+		<div>
+			<AppBar />
+			<MainInfo />
+			<TabsPage />
+		</div>
+	);
 };
 
 export default DoctorDashboardPage;
