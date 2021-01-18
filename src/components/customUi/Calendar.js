ab971a2b06d59e2f351c3d5a0e068c81fee94b66
DamjanKarlovski <damjan_karlovski@hotmail.com>
2021-01-18 10:13:05 +0100

    Calendar touched

--- src/components/customUi/Calendar.js
+++ src/components/customUi/Calendar.js
@@ -7,7 +7,7 @@ import { Box } from '@material-ui/core';
 const CalendarApp = (props) => {
 	// const classes = useStyles();
 	return (
-		<Box borderColor="grey.500" border={1} style={{ width: 250, height: 400 }}>
+		<Box borderColor="grey.500" border={1} style={{ width: 250, height: 400, display: 'flex', justifyContent: 'center'}}>
 			<h1>CALENDAR</h1>
 		</Box>
 	);
