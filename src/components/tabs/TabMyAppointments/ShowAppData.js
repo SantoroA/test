a95e8de3556efd763ae8ab380f84ec70fb9f1c1d
Aline <alinemcsantoro@gmail.com>
2021-02-09 14:47:19 -0300

    fix: error handling on PatReserveScreen

--- src/components/tabs/TabMyAppointments/ShowAppData.js
+++ src/components/tabs/TabMyAppointments/ShowAppData.js
@@ -5,30 +5,28 @@ import CardAppointment from '../../groups/CardAppointment';
 const ShowAppData = ({ appointments, setDialogAppDetailOpen, setAppointmentToView }) => {
 	return appointments.map((apt) => {
 		return (
-			<div>
-				<CardAppointment
-					onSubmit={() => {
-						setDialogAppDetailOpen(true);
-						setAppointmentToView(apt);
-					}}
-					key={apt._id}
-					showPrice={false}
-					state={{
-						appointment: {
-							amount: apt.amount,
-							end: apt.appointmentTimeEnd,
-							id: apt.profilePatientid._id,
-							idApt: apt._id,
-							start: apt.appointmentTimeStart
-						},
-						name: `${apt.profilePatientid.firstName} ${apt.profilePatientid.lastName}`,
-						pic: apt.accountPatientid.profilePicture,
-						// apt.accountPatientid.profilePicture
-						buttonText: 'View',
-						title: 'Patient'
-					}}
-				/>
-			</div>
+			<CardAppointment
+				onSubmit={() => {
+					setDialogAppDetailOpen(true);
+					setAppointmentToView(apt);
+				}}
+				key={apt._id}
+				showPrice={false}
+				state={{
+					appointment: {
+						amount: apt.amount,
+						end: apt.appointmentTimeEnd,
+						id: apt.profilePatientid._id,
+						idApt: apt._id,
+						start: apt.appointmentTimeStart
+					},
+					name: `${apt.profilePatientid.firstName} ${apt.profilePatientid.lastName}`,
+					pic: apt.accountPatientid.profilePicture,
+					// apt.accountPatientid.profilePicture
+					buttonText: 'View',
+					title: 'Patient'
+				}}
+			/>
 		);
 	});
 };
--- src/components/tabs/TabPatientAppointments/ShowAppData.js
+++ src/components/tabs/TabPatientAppointments/ShowAppData.js
@@ -1,36 +1,33 @@
 import React from 'react';
-
 import CardAppointment from '../../groups/CardAppointment';
 
 const ShowAppData = ({ appointments, setDialogAppDetailOpen, setAppointmentToView }) => {
 	console.log(appointments);
 	return appointments.map((apt) => {
 		return (
-			<div>
-				<CardAppointment
-					onSubmit={() => {
-						setDialogAppDetailOpen(true);
-						setAppointmentToView(apt);
-					}}
-					key={apt._id}
-					showPrice={true}
-					state={{
-						appointment: {
-							amount: apt.amount,
-							end: apt.appointmentTimeEnd,
-							id: apt.profilePatientid._id,
-							idApt: apt._id,
-							start: apt.appointmentTimeStart
-						},
-						name: 'Doctor Fulano',
-						pic:
-							'https://images.pexels.com/photos/3985163/pexels-photo-3985163.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-						// apt.accountPatientid.profilePicture
-						buttonText: 'View',
-						title: 'Doctor'
-					}}
-				/>
-			</div>
+			<CardAppointment
+				onSubmit={() => {
+					setDialogAppDetailOpen(true);
+					setAppointmentToView(apt);
+				}}
+				key={apt._id}
+				showPrice={true}
+				state={{
+					appointment: {
+						amount: apt.amount,
+						end: apt.appointmentTimeEnd,
+						id: apt.profilePatientid._id,
+						idApt: apt._id,
+						start: apt.appointmentTimeStart
+					},
+					name: 'Doctor Fulano',
+					pic:
+						'https://images.pexels.com/photos/3985163/pexels-photo-3985163.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+					// apt.accountPatientid.profilePicture
+					buttonText: 'View',
+					title: 'Doctor'
+				}}
+			/>
 		);
 	});
 };
